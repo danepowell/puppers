f6659932e01df0f75343be84cc68495502b66fb0
Dane Powell <git@danepowell.com>
2014-11-12 13:26:14 -0600

    Moved info.php.

--- manifests/site.pp
+++ manifests/site.pp
@@ -18,7 +18,7 @@ class { '::mysql::server':
 }
 package { 'php5': }
 # ensure info.php file exists
-file { '/var/www/info.php':
+file { '/var/www/html/info.php':
   ensure => file,
   content => '<?php  phpinfo(); ?>',    # phpinfo code
   require => Package['apache2'],        # require 'apache2' package before creating
